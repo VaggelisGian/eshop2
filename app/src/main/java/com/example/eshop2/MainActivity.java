3b2adce4df815cdc61245ae196ff2ff199c63109
Vaggelis <baggelis100@gmail.com>
2023-04-12 14:36:14 +0300

    yes

--- app/src/main/java/com/example/eshop2/MainActivity.java
+++ app/src/main/java/com/example/eshop2/MainActivity.java
@@ -10,6 +10,7 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+
 import com.example.eshop2.ui.product.AddEditProductActivity;
 import com.example.eshop2.ui.product.ProductAdapter;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
